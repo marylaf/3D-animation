a734c617e8d531d3538acccaa8b9a8ba97df795f
Maria Lafutkina <lafutkina.masha@yandex.ru>
2023-09-21 22:05:01 +0300

    feature: to add submit form

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,19 @@
+const cors = require('cors');
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
 const coneRouter = require('./routes/cone');
 
 const app = express();
-mongoose.connect('mongodb://127.0.0.1:27017/conedb', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
 
 const { PORT = 3000 } = process.env;
 
+app.use(cors());
+
 app.use(bodyParser.json());
 
-app.use('/', userRouter);
+app.use('/', coneRouter);
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
   });
+  
